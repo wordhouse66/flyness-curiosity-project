30546ed5128ad12d102f51aea43fd9a8ab8e5ed8
wordhouse66 <wordhouse66@yahoo.com>
2025-07-22 16:44:43 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/your-repo-name/',
   server: {
     host: "::",
     port: 8080,
@@ -20,3 +21,6 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
+
+
